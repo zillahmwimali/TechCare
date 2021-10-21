cea5fc93aeb2cce7fd684ade7d0d3a6754751b41
Catherinegichina <catherinegichinaa@gmail.com>
2021-10-21 10:18:15 +0300

    landing page

--- Desktop/DjagoTechCare/techcare/chw/urls.py
+++ Desktop/DjagoTechCare/techcare/chw/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.urls import path
-from .views import index
+from .views import login
 
 urlpatterns = [
-    path('',index,name='index')
+    path('login/',login,name='login')
 ]
--- Desktop/DjagoTechCare/techcare/techcare/settings.py
+++ Desktop/DjagoTechCare/techcare/techcare/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'chw'
+    'chw',
+    'core',
 ]
 # ACCOUNT_ACTIVATION_DAYS = 7
 # REGISTRATION_AUTO_LOGIN = True
