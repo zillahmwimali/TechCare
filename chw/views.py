523954f03be9f289a00f420b3d2744637afffedd
zillahmwimali <catherinegichinaa@gmail.com>
2021-11-09 14:34:09 +0300

    media querries,chw profile,referral form,referral list,follow up page

---
+++ chw/views.py
@@ -0,0 +1,103 @@
+from django.shortcuts import render, redirect
+from .models import Households,Referral
+from .forms import HouseholdRegistrationForm,ReferralRegistrationForm
+from django.contrib import messages
+from django.urls import reverse             
+
+
+
+def addHousehold(request):
+
+    if request.method == "POST":
+        form = HouseholdRegistrationForm(request.POST, request.FILES)
+        if form.is_valid():
+            messages.success(request, 'Successfully registered' +
+                             str(Households.contact_person_first_name))
+            form.save()
+            return redirect('household')
+        else:
+            print(form.errors)
+    else:
+        form = HouseholdRegistrationForm()
+    return render(request, 'addhousehold.html', {'form': form})
+
+
+def chwHouseholds(request):
+    households = Households.objects.all()
+    return render(request, 'household.html', {'households': households})
+
+
+def edit_household(request,id):
+    household = Households.objects.get(id=id)
+    if request.method=='POST':
+        form = HouseholdRegistrationForm(request.POST,instance = household )
+        if form.is_valid():
+            form.save()
+            return redirect('household')
+
+    else:
+        form = HouseholdRegistrationForm(instance = household )
+        return render(request,'edit_household.html',{'form':form}) 
+def refer(request):
+
+    if request.method == "POST":
+        form = ReferralRegistrationForm(request.POST, request.FILES)
+        if form.is_valid():
+            messages.success(request,'Successfully reffered' + str(Referral.patients_first_name))
+            form.save()
+            return redirect('follow_up')
+        else:
+            print (form.errors)
+    else:
+        form = ReferralRegistrationForm()
+    return render(request,'referral.html',{'form':form})
+
+def follow_up(request):
+    referrees = Referral.objects.all()
+    return render(request,'follow_up.html',{'referrees': referrees})
+
+
+def delete_referree(request,id):
+    referral = Referral.objects.get(id=id)
+    referral.delete()
+    return redirect(reverse('follow_up'))
+
+
+
+def edit_referral(request,id):
+    referral = Referral.objects.get(id=id)
+    if request.method=='POST':
+        form = ReferralRegistrationForm(request.POST,instance = referral )
+        if form.is_valid():
+            form.save()
+            return redirect('follow_up')
+
+    else:
+        form = ReferralRegistrationForm(instance = referral )
+    return render(request,'edit_referral.html',{'form':form}) 
+
+
+
+def assessments(request):
+    return render(request, 'patient_assessments.html')
+
+
+def referrals(request):
+    return render(request, 'Referrals.html')
+
+
+def followUp(request):
+    return render(request, 'follow-up.html')
+
+def new_household(request):
+    return render(request, 'new_household.html')
+
+def general_assessment(request):
+    return render(request,'general_assessment.html')
+
+def child_assess(request):
+    return render(request,'child_assess.html')
+
+
+def mother_assess(request):
+    return render(request,'mother_assess.html')
---
+++ core/static/js/home.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+
+});
+new TypeIt("#type2", {
+    speed: 120,
+    loop: true,
+    waitUntilVisible: true,
+  })
+    .type("Learn continually,", { delay: 400 })
+    .pause(500)
+    .delete(18)
+    .type("There's always 'one more thing' to learn", { delay: 400 })
+    .pause(500)
+    .delete(9)
+    .go();
+
+
+
