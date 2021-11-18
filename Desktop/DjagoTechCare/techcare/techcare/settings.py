1a924c38bb955c641f3d7ff565d23902f6aadea8
zillahmwimali <zillahmwimmali@gmail.com>
2021-11-18 16:13:15 +0300

    added procfile and requirements

--- Desktop/DjagoTechCare/techcare/techcare/settings.py
+++ Desktop/DjagoTechCare/techcare/techcare/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-psvk9_958cqal&5sph@x&^de6bfv0b7z=$ol@n*!+y6$=!z)jk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0','127.0.0.1', 'techcareappl.herokuapp.com']
 
 
 # Application definition
--- Desktop/DjagoTechCare/techcare/techcare/urls.py
+++ Desktop/DjagoTechCare/techcare/techcare/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+
 
 urlpatterns = [
     path ( '',include('core.urls')),
@@ -26,4 +29,7 @@ urlpatterns = [
     path ('event/',include('event.urls')),
     path ('contact/',include('contact.urls')),
 
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] 
+urlpatterns += staticfiles_urlpatterns()
+
+
